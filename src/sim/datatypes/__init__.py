5bc73a917ae33f637319e5ca2608d32f44a2ed22
Patrick Rowe <patrick.william.rowe@gmail.com>
2023-02-22 20:28:09 -0800

    Numba optimisation of _update_pos() and _update_coord() functions.

--- src/sim/datatypes/__init__.py
+++ src/sim/datatypes/__init__.py
@@ -4,6 +4,7 @@ import attr
 
 from src.config.sim_conf import sconf
 
+import numba
 
 @attr.define
 class SimPos:
@@ -17,27 +18,34 @@ class SimPos:
         return (self.x, self.y)
 
     def update_pos(self, vec: Tuple[float, float]) -> None:
-        """Update the position and the direction vector based on the move."""
+        self.x, self.y, self.vec = _update_pos(x = self.x, y = self.y, vec = vec, boundary_x=sconf.default_map_resolution_x, boundary_y=sconf.default_map_resolution_y)
 
-        old_x, old_y = self.x, self.y
-        self.x = self._update_coord(self.x, vec[0], sconf.sim_x)
-        self.y = self._update_coord(self.y, vec[1], sconf.sim_y)
-        self.vec = (self.x - old_x, self.y - old_y)
+@numba.jit
+def _update_pos(x: float, y: float, vec: Tuple[float, float], boundary_x: int, boundary_y: int) -> Tuple[float, float]:
+    """Update the position and the direction vector based on the move."""
 
-    def _update_coord(
-        self, val: float, step: float, boundary: int
-    ) -> Tuple[float, bool]:
-        """Update the coordinates with some vector"""
+    old_x, old_y = x, y
+    x = _update_coord(x, vec[0], boundary_x)
+    y = _update_coord(y, vec[1], boundary_y)
+    vec = (x - old_x, y - old_y)
 
-        boundary -= 1
+    return x, y, vec
 
-        newpos = val + step
+@numba.jit
+def _update_coord(
+    val: float, step: float, boundary: int
+) -> Tuple[float, bool]:
+    """Update the coordinates with some vector"""
 
-        if 0 <= newpos <= boundary:
-            return newpos
-        elif newpos < 0:
-            return 0
-        elif newpos > boundary:
-            return boundary
-        else:
-            raise ValueError(f"Entity has produced nonsensical new position: {newpos}")
+    boundary -= 1
+
+    newpos = val + step
+
+    if 0 <= newpos <= boundary:
+        return newpos
+    elif newpos < 0:
+        return 0
+    elif newpos > boundary:
+        return boundary
+    else:
+        return None
